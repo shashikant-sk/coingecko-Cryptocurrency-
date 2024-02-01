5607aa8219ccbf750a1862672d3d8e9cf41b85f5
shashikant-sk <shashikantshah540@gmail.com>
2024-02-02 04:01:30 +0530

    push

--- src/app/Components/navbar.tsx
+++ src/app/Components/navbar.tsx
@@ -13,15 +13,20 @@ const Navbar: React.FC<NavbarProps> = (props) => {
   const { coins, exchanges, marketCap, vol24h, dominance, gas } = props;
 
   return (
-    <nav className="bg-white text-black flex space-x-3">
-      <span className="flex text-xs">Coins: {coins}</span>
+    <nav className="bg-white text-black flex space-x-3 pt-4 pb-4 border pl-5 border-gray-300">
+      <span className="flex text-xs text-zinc-600 ">Coins: {coins}</span>
       <span className="flex text-xs">Exchanges: {exchanges}</span>
       <span className="flex text-xs">Market Cap: {marketCap}</span>
       <span className="flex text-xs">24h Vol: {vol24h}</span>
       <span className="flex text-xs">Dominance: {dominance}</span>
       <span className="flex text-xs">ETH 17.4% Gas: {gas}</span>
     </nav>
-  );
+    
+    
+    
+    
+    );
 };
+
 
 export default Navbar;
